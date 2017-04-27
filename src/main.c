b451be8dfadd874c2eb96c90dc1d420a25160967
Berdnikov Evgeni <jon555jon@mail.ru>
2017-04-27 20:06:44 +0300

    Fixed Count and Parse.

--- src/main.c
+++ src/main.c
@@ -31,7 +31,8 @@ typedef enum
   ERR_DIV_ZERO,
   ERR_BRACKETS,
   ERR_EXPRESSION,
-  ERR_DOMAIN
+  ERR_DOMAIN,
+  ERR_SIGNS
 } error_t;
 // TODO: Move to a separate module 'calc'
 char const* GetErrorString(error_t error)
@@ -52,6 +53,8 @@ char const* GetErrorString(error_t error)
     return "Wrong expression.";
   else if (error == ERR_DOMAIN)
     return "Out of domain.";
+  else if (error == ERR_SIGNS)
+    return "Not enough arithmetic signs.";
   else if (error == ERR_OK)
     return "Redundant error report.";
   else
@@ -123,14 +126,17 @@ int isoper(char s)
     return 2;
   else if (s == '+' || s == '-')
     return 3;
+  else if (s == '(' || s == ')')
+    return 4;
   else
     return 0;
 }
 void Parse(char const *expression, char *output, error_t* lastError)
 {
-  int i = 0, j = 0, k = 0;
-  char *stack;
-  stack = (char*)malloc(sizeof(char)*strlen(expression)/2+1);//realloc
+  int i = 0, j = 0, k = 0, n, previous = 0;
+  char *stack, *realltmp;
+  n = 8 * MEM_BLOCK;
+  stack = (char*)malloc(sizeof(char)*n);
   if (stack == NULL)
   {
     *lastError = ERR_NOT_ENOUGH_MEMORY;
@@ -140,28 +146,36 @@ void Parse(char const *expression, char *output, error_t* lastError)
   {
     if (isdigit(expression[i]))
     {
+      if (previous == 1)
+      {
+        *lastError = ERR_SIGNS;
+        return;
+      }
       output[j] = expression[i];
       j++;
       if (!isdigit(expression[i+1]) || isspace(expression[i+1]))
       {
         output[j] = ' ';
         j++;
+        previous = 1;
       }
     }
     else if (isfunc(expression + i))
     {
       FuncToStack(stack[k], isfunc(expression + i));
       k++;
+      previous = 2;
     }
     else if (expression[i] == '(')
     {
       stack[k] = expression[i];
       k++;
+      previous = 3;
     }
     else if (expression[i] == ')')
     {
       k--;
-      if (k < 2)
+      if (k < 1)
       {
         *lastError = ERR_BRACKETS;
         return;
@@ -177,6 +191,7 @@ void Parse(char const *expression, char *output, error_t* lastError)
         *lastError = ERR_BRACKETS;
         return;
       }
+      previous = 4;
     }
     else if (isoper(expression[i]))
     {
@@ -193,6 +208,7 @@ void Parse(char const *expression, char *output, error_t* lastError)
       }
       stack[k] = expression[i];
       k++;
+      previous = 5;
     }
     else if (isspace(expression[i]));
     else
@@ -201,9 +217,20 @@ void Parse(char const *expression, char *output, error_t* lastError)
       return;
     }
     i++;
+    if (k >= n)
+    {
+      n += MEM_BLOCK;
+      if ((realltmp = (char*)realloc(stack, n)) == NULL)
+      {
+        *lastError = ERR_NOT_ENOUGH_MEMORY;
+        return;
+      }
+      else
+        stack = realltmp;
+    }
   }
   k--;
-  if (k>-1)
+  if (isoper(stack[0])||(isdigit(output[j-2])&&!isoper(stack[0])))
     for (; k > -1; k--, j++)
     {
       if (stack[k] == '(')
@@ -223,12 +250,13 @@ void Parse(char const *expression, char *output, error_t* lastError)
   free(stack);
   output[j] = '\0';
 }
-double Count(char* output, error_t* lastError)//spaces!
+double Count(char* output, error_t* lastError)
 {
-  int j = 0;
-  double *stack, number;
+  int j = 0, n;
+  double *stack, *realltmp, number;
   char *endptr;
-  stack = (double*)malloc(sizeof(double)*strlen(output) / 4 + 1);//realloc
+  n = 8 * MEM_BLOCK;
+  stack = (double*)malloc(sizeof(double)*n);
   while (*output)
   {
     if (isdigit(*output))
@@ -278,7 +306,17 @@ double Count(char* output, error_t* lastError)//spaces!
     }
     if (isspace(*output))
       *output++;
-    //i++;
+    if (j >= n)
+    {
+      n += MEM_BLOCK;
+      if ((realltmp = (double*)realloc(stack, n)) == NULL)
+      {
+        *lastError = ERR_NOT_ENOUGH_MEMORY;
+        return 1;
+      }
+      else
+        stack = realltmp;
+    }
   }
   number = stack[0];
   free(stack);
@@ -295,13 +333,11 @@ double Calculate(char const* expression, error_t* lastError)
     return 1;
   }
   Parse(expression, output, lastError);
-  //printf("%s", output);//temp
   if (*lastError != ERR_OK)
     return 1;
   result = Count(output, lastError);
   if (*lastError != ERR_OK)
     return 1;
-  //result = 4.0;
   free(output);
   return result;
 }
@@ -368,7 +404,7 @@ void ProcessLine(char const* line, FILE* in, error_t* lastError)
   double result = Calculate(line, lastError);
   if (*lastError == ERR_OK)
   {
-    printf("%lg", result);
+    printf("%g", result);
     if (!feof(in))
       printf("\n");
   }
@@ -399,11 +435,13 @@ int main(int argc, char const* argv[])
   {
     lastError = ERR_CANNOT_OPEN;
     ReportError(lastError);
+    exit(ERR_CANNOT_OPEN);
   }
   if (argc > 2)
   {
     lastError = ERR_ARGUMENTS;
     ReportError(lastError);
+    exit(ERR_ARGUMENTS);
   }
   while ((line = ReadLine(in, &lastError)) != NULL)// Process the data line by line
   {
